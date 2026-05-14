a294177e138b0a3af13bdf006b3ed3399c591002
kanishka533 <kanishka.s3003@gmail.com>
2026-05-14 23:47:42 +0530

    fixed backend

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-app = Flask(**name**)
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-return "GeneScope Backend Running"
+    return "GeneScope Backend Running"
 
-if **name** == '**main**':
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
